a6a5f2e34664215a2f13fcac2098018f0be0d755
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2020-11-03 18:16:50 +0530

    added middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const Express = require('express')
+const setupMiddleware = require('./setup/middleware')
 
 const app = Express()
+
+setupMiddleware(app)
 
 app.listen(4000, () => {
     console.log('Server started on port 4000')
