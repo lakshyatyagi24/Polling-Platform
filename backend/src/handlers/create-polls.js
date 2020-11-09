69820d467d47a7b6d1ac8b04092478fa54f6aff9
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-10 00:06:14 +0530

    indicative

--- backend/src/handlers/create-polls.js
+++ backend/src/handlers/create-polls.js
@@ -2,6 +2,7 @@ const { response, request } = require("express")
 
 module.exports = (client) => {
     return (request, response) => {
+        console.log(client)
         response.json({
             message: 'It works.'
         })
