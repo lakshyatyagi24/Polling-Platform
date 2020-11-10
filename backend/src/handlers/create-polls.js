d0b349e462702d8b714ed38e3e9cf7fc3846b928
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-10 16:25:22 +0530

    clean up of extra things

--- backend/src/handlers/create-polls.js
+++ backend/src/handlers/create-polls.js
@@ -1,13 +1,16 @@
-const { response, request } = require("express")
+const { v4 } = require("uuid")
 
 module.exports = (client) => {
     return (request, response) => {
         const data = {
+            _id: v4(),
             title: request.body.title,
             choices: request.body.choices.map(choice => ({
                 name: choice,
-                count: 0
+                count: 0,
+                _id: v4()
             }))
         }
+        return response.json(data)
     }
 }
