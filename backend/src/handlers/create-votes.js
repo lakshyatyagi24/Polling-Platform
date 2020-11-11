d53f2a271eb95b6a79d28fc71887165dae10642f
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-11 22:37:06 +0530

    check ip address repeated or not

--- backend/src/handlers/create-votes.js
+++ backend/src/handlers/create-votes.js
@@ -2,12 +2,18 @@ const { promisify } = require('util')
 
 module.exports = (db, redisDb) => {
     const saddAsync = promisify(redisDb.sadd).bind(redisDb)
-    const smembersAsync = promisify(redisDb.smembers).bind(redisDb)
+    const sismemberAsync = promisify(redisDb.sismember).bind(redisDb)
 
     return async (request, response) => {
-        console.log(
-            await smembersAsync(request.params.poll)
-        )
+
+        const isMember = await sismemberAsync(request.params.poll, request.body.ip)
+
+        if(isMember) {
+            return response.status(400).json({
+                message: 'You have already voted for this poll.'
+            })
+        }
+
         const result = await db.collection('polls').updateOne({
             _id: request.params.poll,
             'choices._id': request.body.choice
