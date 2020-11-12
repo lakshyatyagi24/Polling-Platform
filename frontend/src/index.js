f0cce9d3118e55cf1766c94c3397864c84895941
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-13 00:24:36 +0530

    added header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <BrowserRouter>
+      <div className="w-full h-screen">
+        <div className="h-16 w-full bg-gray-800 flex items-center">
+          <div className="container mx-auto px-5">
+            <Link to className="text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3">Home</span>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
+serviceWorker.unregister();
