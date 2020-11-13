a81b6c75e7c95bf7706d4c098cb5342624f42f1f
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-13 20:36:23 +0530

    removed URI from backend

--- backend/src/setup/database.js
+++ backend/src/setup/database.js
@@ -1,14 +1,12 @@
 const Mongodb = require('mongodb')
 const Config = require('../config')
 
-const uri = 'mongodb://localhost/polling-platform'
-
 module.exports = async () => {
     const client = new Mongodb.MongoClient(Config.databaseUri, {
         useUnifiedTopology: true
     })
 
     await client.connect()
-    
+
     return client.db()
 }
