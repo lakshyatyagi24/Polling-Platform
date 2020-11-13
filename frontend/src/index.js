1fa10c94d1c129d85e31d48af6234d8d4887c688
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-11-13 14:47:07 +0530

    added pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePoll from './pages/CreatePoll';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,9 +11,16 @@ ReactDOM.render(
       <div className="w-full h-screen">
         <div className="h-16 w-full bg-gray-800 flex items-center">
           <div className="container mx-auto px-5">
-            <Link to className="text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3">Home</span>
+          <Link to='/' className="text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3">Home</Link>
+          <Link to='/polls/create' className="text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3">Create Poll</Link>
           </div>
         </div>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path="/polls/create" component={CreatePoll} />
+        </Switch>
+
       </div>
     </BrowserRouter>
   </React.StrictMode>,
